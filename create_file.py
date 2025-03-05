0c4acc515c20a286cef6e739948a17ed982bf030
sbiscigl <sbiscigl@amazon.com>
2025-03-05 16:04:35 -0500

    fix test file generation

--- create_file.py
+++ create_file.py
@@ -2,5 +2,6 @@ import os
 
 file_size = 100 * 1024 * 1024  # 100MB in bytes
 
+os.makedirs('test-files', exist_ok=True)
 with open('test-files/100mb.txt', 'wb') as f:
     f.write(os.urandom(file_size))
